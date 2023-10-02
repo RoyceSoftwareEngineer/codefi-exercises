864ae1b59bf0ef6ce82e033a5d19e553dc1c079b
RoyceSoftwareEngineer <140214180+RoyceSoftwareEngineer@users.noreply.github.com>
2023-10-01 22:04:53 -0500

    updates to exercises

--- 083123Exercises.js
+++ 083123Exercises.js
@@ -17,3 +17,33 @@ console.log(result);
 //prints the product to console.
 
 //exercise 3
+const person={
+    name:'Royce',
+    age:38,
+    gender:'Male'
+};
+console.log(person);
+//prints the person properties to the console.
+
+//exercise 4
+let car={
+    make: 'Ford',
+    model: 'Mustang',
+    year: 2003,
+};
+console.log(car);
+//prints properties for car to the console.
+
+//exercise 5
+let str="hello";
+function reverseString(str){
+    var splitString = str.split("");
+    ["h", "e", "l", "l","o"];
+    var reverseArray= splitstring.reverse();
+    ["o","l","l","e","h"];
+    var joinArray= reverseArray.join("");
+    return joinArray;
+
+}
+console.log(str);
+// no idea why this wont work for me.
